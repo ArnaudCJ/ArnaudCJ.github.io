68e43be4fc05de054f6405e6c70df046c67ce030
Arnaud CJ <a.cjouret@tutanota.com>
2019-08-14 16:59:58 +0200

    Do the edit agaiN...

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ let myHeading = document.querySelector('h1');
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {let storedName = localStorage.getItem('name');
-myHeading.textContent = 'Bienvenue sur mon profil GitHub' + storedName '!';
+myHeading.textContent = 'Bienvenue sur mon profil GitHub' + storedName;
 }
 
 /* Déclenchement de la demande du prénom si le test retourne 'false' + stockage dans le navigateur */
 function setUserName() {
   let myName = prompt('Bonjour ! Quel est votre prénom ?')
   localStorage.SetItem('name', myName);
-  myHeading.textContent = 'Bienvenue sur mon profil GitHub' + myName '!';
+  myHeading.textContent = 'Bienvenue sur mon profil GitHub' + myName;
 }
 
 /* Mise en place de l'event ".onclick" sur la variable myButton attachée au bouton dans le html pour déclencher la fonction "setUserName"  */
